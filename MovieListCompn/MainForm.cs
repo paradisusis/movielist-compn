13bb35345a6b0c0f43ce931c8a8bdb5ee5c2b0a0
Victor <paradisusvic@gmail.com>
2024-05-15 22:35:53 -0400

    Add OnFirstListBrowseButtonClick()

--- MovieListCompn/MainForm.cs
+++ MovieListCompn/MainForm.cs
@@ -6,81 +6,103 @@ using System.Windows.Forms;
 
 namespace MovieListCompn
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-		}
-		
-		void OnNewToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnProcessButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnFirstListBrowseButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnSecondListBrowseButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
-		{
-			
-		}
-		
-		void OnMoreReleasesParadisusisToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnMainFormLoad(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnMainFormFormClosing(object sender, FormClosingEventArgs e)
-		{
-			
-		}
-		
-		void OnExitToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-	}
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+        }
+
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnProcessButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Ons the first list browse button click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnFirstListBrowseButtonClick(object sender, EventArgs e)
+        {
+            // Reset file name
+            this.openFileDialog.FileName = string.Empty;
+
+            // Show open file dialog
+            if (this.openFileDialog.ShowDialog() == DialogResult.OK && this.openFileDialog.FileName.Length > 0)
+            {
+                // Set the file into text box
+                this.firstListTextBox.Text = this.openFileDialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Ons the second list browse button click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnSecondListBrowseButtonClick(object sender, EventArgs e)
+        {
+            // Reset file name
+            this.openFileDialog.FileName = string.Empty;
+
+            // Show open file dialog
+            if (this.openFileDialog.ShowDialog() == DialogResult.OK && this.openFileDialog.FileName.Length > 0)
+            {
+                // Set the file into text box
+                this.secondListTextBox.Text = this.openFileDialog.FileName;
+            }
+        }
+
+        private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void OnMoreReleasesParadisusisToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnMainFormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnMainFormFormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
